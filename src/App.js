018a342716f9e1ab94898c78edec16b5aa90ec18
Nithin Kota <Nithin@Rajinis-MacBook-Pro.local>
2021-06-13 15:25:10 -0400

    Changed Ipublic/index.html so that App.js has 100% width and height Added Components folder in /src and contains Dashboard.js and Navbar.js Dashborad will contain all slides and navbar Navbar has forward and backward buttons Also added App.css in /src and has style for Components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Dashboard from "./Components/Dashboard";
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div id="navbar-container">
+      <button id="backward-button">{"<"}</button>
+      <button id="forward-button">{">"}</button>
+    </div>
+  );
+}
